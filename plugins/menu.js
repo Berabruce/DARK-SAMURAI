bf113f75c504b85431eb5b74ed7806f80138d974
Bera <yangujina73@gmail.com>
2025-03-08 10:19:45 +0300

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -1,5 +1,3 @@
-
-
 import moment from 'moment-timezone';
 import fs from 'fs';
 import os from 'os';
@@ -7,54 +5,80 @@ import pkg from '@whiskeysockets/baileys';
 const { generateWAMessageFromContent, proto } = pkg;
 import config from '../config.cjs';
 
-cmd({
-    pattern: "menu",
-    alias: ["allmenu","fullmenu"],use: '.menu2',
-    desc: "menu the bot",
-    category: "menu",
-    react: "⚡",
-    filename: __filename
-}, 
-async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
-    try {
-        let dec = `╭━━━〔 *${config.BOT_NAME}* 〕━━━┈⊷
-╭━━━━━━━━━━━━━━━━━┈⊷
+// Get total memory and free memory in bytes
+const totalMemoryBytes = os.totalmem();
+const freeMemoryBytes = os.freemem();
+
+// Define unit conversions
+const byteToKB = 1 / 1024;
+const byteToMB = byteToKB / 1024;
+const byteToGB = byteToMB / 1024;
+
+// Function to format bytes to a human-readable format
+function formatBytes(bytes) {
+  if (bytes >= Math.pow(1024, 3)) {
+    return (bytes * byteToGB).toFixed(2) + ' GB';
+  } else if (bytes >= Math.pow(1024, 2)) {
+    return (bytes * byteToMB).toFixed(2) + ' MB';
+  } else if (bytes >= 1024) {
+    return (bytes * byteToKB).toFixed(2) + ' KB';
+  } else {
+    return bytes.toFixed(2) + ' bytes';
+  }
+}
+
+// Bot Process Time
+const uptime = process.uptime();
+const day = Math.floor(uptime / (24 * 3600)); // Calculate days
+const hours = Math.floor((uptime % (24 * 3600)) / 3600); // Calculate hours
+const minutes = Math.floor((uptime % 3600) / 60); // Calculate minutes
+const seconds = Math.floor(uptime % 60); // Calculate seconds
+
+// Uptime
+const uptimeMessage = `*I am alive now since ${day}d ${hours}h ${minutes}m ${seconds}s*`;
+const runMessage = `*☀️ ${day} Day*\n*🕐 ${hours} Hour*\n*⏰ ${minutes} Minutes*\n*⏱️ ${seconds} Seconds*\n`;
+
+const xtime = moment.tz("Asia/Colombo").format("HH:mm:ss");
+const xdate = moment.tz("Asia/Colombo").format("DD/MM/YYYY");
+const time2 = moment().tz("Asia/Colombo").format("HH:mm:ss");
+let pushwish = "";
+
+if (time2 < "05:00:00") {
+  pushwish = `Good Morning 🌄`;
+} else if (time2 < "11:00:00") {
+  pushwish = `Good Morning 🌄`;
+} else if (time2 < "15:00:00") {
+  pushwish = `Good Afternoon 🌅`;
+} else if (time2 < "18:00:00") {
+  pushwish = `Good Evening 🌃`;
+} else if (time2 < "19:00:00") {
+  pushwish = `Good Evening 🌃`;
+} else {
+  pushwish = `Good Night 🌌`;
+}
+
+const test = async (m, Matrix) => {
+  const prefix = config.PREFIX;
+  const cmd = m.body.startsWith(prefix) ? m.body.slice(prefix.length).split(' ')[0].toLowerCase() : '';
+  const mode = config.MODE === 'public' ? 'public' : 'private';
+  const pref = config.PREFIX;
+
+  const validCommands = ['list', 'help', 'menu'];
+
+  if (validCommands.includes(cmd)) {
+    const str = `╭━━━〔 *𝖣𝖠𝖱𝖪 𝖲𝖠𝖬𝖴𝖱𝖠𝖨* 〕━━━┈⊷
 ┃★╭──────────────
-┃★│ 𝔻𝔼𝕍𝔼𝕃𝕆ℙ𝔼ℝ : *BRUCE BERA*
-┃★│ 𝕌𝕊𝔼ℝ :  ${pushName} ${pushwish}
-┃★│ 𝔻𝔼𝕍 ℕ𝕆 : *Multi Device*
-┃★│ 𝕋𝕐ℙ𝔼 : *NodeJs*
-┃★│ 𝕄𝕆𝔻𝔼 : *${mode}*
-┃★│ ℙℝ𝔼𝔽𝕀𝕏 : [${prefix}]
-┃★│ 𝔹𝕆𝕋 𝕍𝔼ℝ𝕊𝕀𝕆ℕ : *1.1.0*
+┃★│ Owner : *𝖡𝖱𝖴𝖢𝖤 𝖡𝖤𝖱𝖠*
+┃★│ User : *${m.pushName}*
+┃★│ Baileys : *ᴍᴜʟᴛʏ-ᴅᴇᴠɪᴄᴇ*
+┃★│ Type : *ɴᴏᴅᴇ.ᴊs*
+┃★│ Mode : *${mode}*
+┃★│ Platform : *${os.platform()}*
+┃★│ Prefix : [${prefix}]
+┃★│ Version : *1.0.0*
 ┃★╰──────────────
 ╰━━━━━━━━━━━━━━━┈⊷ 
-> ℍ𝔼𝕃𝕃𝕆 ${pushName} ${pushwish}
- ╭───❍「 *𝖡𝖤𝖱𝖠 𝖳𝖤𝖢𝖧 𝖡𝖮𝖳* 」
-*┋* ©️ *${prefix}𝗅𝗈𝗀𝗈*
-*┋* ©️ *${prefix}𝖻𝗅𝖺𝖼𝗄𝗉𝗂𝗇𝗄*
-*┋* ©️ *${prefix}𝗀𝗈𝗌𝗌𝗒𝗌𝗂𝗅𝗏𝖾𝗋*
-*┋* ©️ *${prefix}𝗇𝖺𝗋𝗎𝗋𝗈*
-*┋* ©️ *${prefix}𝖽𝗂𝗀𝗂𝗍𝖺𝗅𝗀𝗅𝗂𝗍𝖼𝗁*
-*┋* ©️ *${prefix}𝗉𝗂𝗑𝖾𝗅𝗀𝗅𝗂𝗍𝖼𝗁*
-*┋* ©️ *${prefix}𝗌𝗍𝖺𝗋*
-*┋* ©️ *${prefix}𝗌𝗆𝗈𝗄𝖾*
-*┋* ©️ *${prefix}𝖻𝖾𝖺𝗋*
-*┋* ©️ *${prefix}𝗇𝖾𝗈𝗇𝖽𝖾𝗏𝗂𝗅*
-*┋* ©️ *${prefix}𝗌𝖼𝗋𝖾𝖾𝗇*
-*┋* ©️ *${prefix}𝗇𝖺𝗍𝗎𝗋𝖾*
-*┋* ©️ *${prefix}𝖽𝗋𝖺𝗀𝗈𝗇𝖻𝖺𝗅𝗅*
-*┋* ©️ *${prefix}𝖿𝗈𝗀𝗀𝗒𝗀𝗅𝖺𝗌𝗌*
-*┋* ©️ *${prefix}𝗇𝖾𝗈𝗇𝗅𝗂𝗀𝗁𝗍*
-*┋* ©️ *${prefix}𝖼𝖺𝗌𝗍𝗅𝖾𝗉𝗈𝗉*
-*┋* ©️ *${prefix}𝖿𝗋𝗈𝗓𝖾𝗇𝖼𝗁𝗋𝗂𝗌𝗍𝗆𝖺𝗌*
-*┋* ©️ *${prefix}𝖿𝗈𝗂𝗅𝖻𝖺𝗅𝗅𝗈𝗈𝗇*
-*┋* ©️ *${prefix}𝖼𝗈𝗅𝗈𝗋𝖿𝗎𝗅𝗉𝖺𝗂𝗇𝗍*
-*┋* ©️ *${prefix}𝖺𝗆𝖾𝗋𝗂𝖼𝖺𝗇𝖿𝗅𝖺𝗀*
-*┋* ©️ *${prefix}𝗇𝖾𝗈𝗇𝖽𝖾𝗏𝗂𝗅*
-╭───────────❍
-│ 𝖱𝖤𝖦𝖠𝖱𝖣𝖲 𝖡𝖱𝖴𝖢𝖤 𝖡𝖤𝖱𝖠
-╰───────────❍
+> *ʜᴇʏ ${m.pushName} ${pushwish}*
 ╭━❮ 𝙲𝙾𝙽𝚅𝙴𝚁𝚃𝙴𝚁 ❯━╮
 ┃◈ ${prefix}𝙰𝚃𝚃𝙿
 ┃◈ ${prefix}𝙰𝚃𝚃𝙿2
@@ -64,6 +88,31 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
 ┃◈ ${prefix}𝙴𝙼𝙾𝙹𝙸𝙼𝙸𝚇
 ┃◈ ${prefix}𝙼𝙿3
 ╰━━━━━━━━━━━━━━━⪼
+ ╭━❮ ʟᴏɢᴏ ᴍᴇɴᴜ ❯━╮
+*┋*  ${prefix}𝗅𝗈𝗀𝗈
+*┋*  ${prefix}𝖻𝗅𝖺𝖼𝗄𝗉𝗂𝗇𝗄
+*┋*  ${prefix}𝗀𝗈𝗌𝗌𝗒𝗌𝗂𝗅𝗏𝖾𝗋
+*┋*  ${prefix}𝗇𝖺𝗋𝗎𝗋𝗈
+*┋*  ${prefix}𝖽𝗂𝗀𝗂𝗍𝖺𝗅𝗀𝗅𝗂𝗍𝖼𝗁
+*┋*  ${prefix}𝗉𝗂𝗑𝖾𝗅𝗀𝗅𝗂𝗍𝖼𝗁
+*┋*  ${prefix}𝗌𝗍𝖺𝗋
+*┋*  ${prefix}𝗌𝗆𝗈𝗄𝖾
+*┋*  ${prefix}𝖻𝖾𝖺𝗋*
+*┋*  ${prefix}𝗇𝖾𝗈𝗇𝖽𝖾𝗏𝗂𝗅
+*┋*  ${prefix}𝗌𝖼𝗋𝖾𝖾𝗇
+*┋*. ${prefix}𝗇𝖺𝗍𝗎𝗋𝖾
+*┋*  ${prefix}𝖽𝗋𝖺𝗀𝗈𝗇𝖻𝖺𝗅𝗅
+*┋*  ${prefix}𝖿𝗈𝗀𝗀𝗒𝗀𝗅𝖺𝗌𝗌
+*┋*  ${prefix}𝗇𝖾𝗈𝗇𝗅𝗂𝗀𝗁𝗍
+*┋*  ${prefix}𝖼𝖺𝗌𝗍𝗅𝖾𝗉𝗈𝗉
+*┋*  ${prefix}𝖿𝗋𝗈𝗓𝖾𝗇𝖼𝗁𝗋𝗂𝗌𝗍𝗆𝖺𝗌
+*┋*  ${prefix}𝖿𝗈𝗂𝗅𝖻𝖺𝗅𝗅𝗈𝗈𝗇
+*┋*  ${prefix}𝖼𝗈𝗅𝗈𝗋𝖿𝗎𝗅𝗉𝖺𝗂𝗇𝗍
+*┋*  ${prefix}𝖺𝗆𝖾𝗋𝗂𝖼𝖺𝗇𝖿𝗅𝖺𝗀
+*┋*  ${prefix}𝗇𝖾𝗈𝗇𝖽𝖾𝗏𝗂𝗅
+╭───────────❍
+│ʀᴇɢᴀʀᴅs ʙᴇʀᴀ ᴛᴇᴄʜ
+╰───────────❍
 ╭━❮ 𝙰𝙸 ❯━╮
 ┃◈ ${prefix}𝙰𝚒
 ┃◈ ${prefix}𝙱𝚞𝚐
@@ -153,38 +202,32 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
 ┃◈ ${prefix}𝚃𝚛𝚞𝚎𝚌𝚊𝚕𝚕𝚎𝚛
 ┃◈ ${prefix}𝙸𝚗𝚜𝚝𝚊𝚂𝚝𝚊𝚕𝚔
 ┃◈ ${prefix}𝙶𝚒𝚝𝚑𝚞𝚋𝚂𝚝𝚊𝚕𝚔
-╰━━━━━━━━━━━━━━━⪼
-   
-> ${config.DESCRIPTION}`;
+╰━━━━━━━━━━━━━━━⪼`;
 
-        await conn.sendMessage(
-            from,
-            {
-                image: { url: `https://files.catbox.moe/7xgzln.jpg` },
-                caption: dec,
-                contextInfo: {
-                    mentionedJid: [m.sender],
-                    forwardingScore: 999,
-                    isForwarded: true,
-                    forwardedNewsletterMessageInfo: {
-                        newsletterJid: '120363315115438245@newsletter',
-                        newsletterName: 'BERA TECH',
-                        serverMessageId: 143
-                    }
-                }
-            },
-            { quoted: mek }
-        );
+    await Matrix.sendMessage(m.from, {
+      image: fs.readFileSync('./media/samurai.jpg'),
+      caption: str,
+      contextInfo: {
+        mentionedJid: [m.sender],
+        forwardingScore: 999,
+        isForwarded: true,
+        forwardedNewsletterMessageInfo: {
+          newsletterJid: '120363315115438245@newsletter',
+          newsletterName: "ʀᴇɢᴀʀᴅs ʙᴇʀᴀ ᴛᴇᴄʜ",
+          serverMessageId: 143
+        }
+      }
+    }, {
+      quoted: m
+    });
 
-        // Send audio
-        await conn.sendMessage(from, {
-            audio: { url: 'https://files.catbox.moe/nk3nuz.mp3' },
-            mimetype: 'audio/mp4',
-            ptt: true
-        }, { quoted: mek });
-        
-    } catch (e) {
-        console.log(e);
-        reply(`${e}`);
-    }
-});
+    // Send audio after sending the menu
+    await Matrix.sendMessage(m.from, {
+      audio: { url: 'https://files.catbox.moe/nk3nuz.mp3' },
+      mimetype: 'audio/mp4',
+      ptt: true
+    }, { quoted: m });
+  }
+};
+
+export default test;

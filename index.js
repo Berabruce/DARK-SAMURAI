0d2d8b7046de5a811de11274cdffcf46759969aa
Bera <yangujina73@gmail.com>
2025-03-08 15:45:18 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,180 +1,237 @@
-import dotenv from 'dotenv';
-dotenv.config();
-
-import {
-    makeWASocket,
-    Browsers,
-    fetchLatestBaileysVersion,
-    DisconnectReason,
-    useMultiFileAuthState,
-} from '@whiskeysockets/baileys';
-import { Handler, Callupdate, GroupUpdate } from './data/index.js';
-import express from 'express';
-import pino from 'pino';
-import fs from 'fs';
-import NodeCache from 'node-cache';
-import path from 'path';
-import chalk from 'chalk';
 import moment from 'moment-timezone';
-import axios from 'axios';
-import config from './config.cjs';
-import pkg from './lib/autoreact.cjs';
-const { emojis, doReact } = pkg;
+import fs from 'fs';
+import os from 'os';
+import pkg from '@whiskeysockets/baileys';
+const { generateWAMessageFromContent, proto } = pkg;
+import config from '../config.cjs';
 
-const sessionName = "session";
-const app = express();
-const orange = chalk.bold.hex("#FFA500");
-const lime = chalk.bold.hex("#32CD32");
-let useQR = false;
-let initialConnection = true;
-const PORT = process.env.PORT || 3000;
+// Get total memory and free memory in bytes
+const totalMemoryBytes = os.totalmem();
+const freeMemoryBytes = os.freemem();
 
-const MAIN_LOGGER = pino({
-    timestamp: () => `,"time":"${new Date().toJSON()}"`
-});
-const logger = MAIN_LOGGER.child({});
-logger.level = "trace";
+// Define unit conversions
+const byteToKB = 1 / 1024;
+const byteToMB = byteToKB / 1024;
+const byteToGB = byteToMB / 1024;
 
-const msgRetryCounterCache = new NodeCache();
-
-const __filename = new URL(import.meta.url).pathname;
-const __dirname = path.dirname(__filename);
-
-const sessionDir = path.join(__dirname, 'session');
-const credsPath = path.join(sessionDir, 'creds.json');
-
-if (!fs.existsSync(sessionDir)) {
-    fs.mkdirSync(sessionDir, { recursive: true });
+// Function to format bytes to a human-readable format
+function formatBytes(bytes) {
+  if (bytes >= Math.pow(1024, 3)) {
+    return (bytes * byteToGB).toFixed(2) + ' GB';
+  } else if (bytes >= Math.pow(1024, 2)) {
+    return (bytes * byteToMB).toFixed(2) + ' MB';
+  } else if (bytes >= 1024) {
+    return (bytes * byteToKB).toFixed(2) + ' KB';
+  } else {
+    return bytes.toFixed(2) + ' bytes';
+  }
 }
 
-async function downloadSessionData() {
-    if (!config.SESSION_ID) {
-        console.error('Please add your session to SESSION_ID env !!');
-        return false;
-    }
-    const sessdata = config.SESSION_ID.split("JOEL~XMD~")[1];
-    const url = `https://pastebin.com/raw/${sessdata}`;
-    try {
-        const response = await axios.get(url);
-        const data = typeof response.data === 'string' ? response.data : JSON.stringify(response.data);
-        await fs.promises.writeFile(credsPath, data);
-        console.log("🔒 Session Successfully Loaded !!");
-        return true;
-    } catch (error) {
-       // console.error('Failed to download session data:', error);
-        return false;
-    }
+// Bot Process Time
+const uptime = process.uptime();
+const day = Math.floor(uptime / (24 * 3600)); // Calculate days
+const hours = Math.floor((uptime % (24 * 3600)) / 3600); // Calculate hours
+const minutes = Math.floor((uptime % 3600) / 60); // Calculate minutes
+const seconds = Math.floor(uptime % 60); // Calculate seconds
+
+// Uptime
+const uptimeMessage = `*I am alive now since ${day}d ${hours}h ${minutes}m ${seconds}s*`;
+const runMessage = `*☀️ ${day} Day*\n*🕐 ${hours} Hour*\n*⏰ ${minutes} Minutes*\n*⏱️ ${seconds} Seconds*\n`;
+
+const xtime = moment.tz("Asia/Colombo").format("HH:mm:ss");
+const xdate = moment.tz("Asia/Colombo").format("DD/MM/YYYY");
+const time2 = moment().tz("Asia/Colombo").format("HH:mm:ss");
+let pushwish = "";
+
+if (time2 < "05:00:00") {
+  pushwish = `Good Morning 🌄`;
+} else if (time2 < "11:00:00") {
+  pushwish = `Good Morning 🌄`;
+} else if (time2 < "15:00:00") {
+  pushwish = `Good Afternoon 🌅`;
+} else if (time2 < "18:00:00") {
+  pushwish = `Good Evening 🌃`;
+} else if (time2 < "19:00:00") {
+  pushwish = `Good Evening 🌃`;
+} else {
+  pushwish = `Good Night 🌌`;
 }
 
-async function start() {
-    try {
-        const { state, saveCreds } = await useMultiFileAuthState(sessionDir);
-        const { version, isLatest } = await fetchLatestBaileysVersion();
-        console.log(`demon-slayer using WA v${version.join('.')}, isLatest: ${isLatest}`);
-        
-        const Matrix = makeWASocket({
-            version,
-            logger: pino({ level: 'silent' }),
-            printQRInTerminal: useQR,
-            browser: ["demon", "safari", "3.3"],
-            auth: state,
-            getMessage: async (key) => {
-                if (store) {
-                    const msg = await store.loadMessage(key.remoteJid, key.id);
-                    return msg.message || undefined;
-                }
-                return { conversation: "demon-slayer whatsapp user bot" };
-            }
-        });
+const test = async (m, Matrix) => {
+  const prefix = config.PREFIX;
+  const cmd = m.body.startsWith(prefix) ? m.body.slice(prefix.length).split(' ')[0].toLowerCase() : '';
+  const mode = config.MODE === 'public' ? 'public' : 'private';
+  const pref = config.PREFIX;
 
-        Matrix.ev.on('connection.update', (update) => {
-            const { connection, lastDisconnect } = update;
-            if (connection === 'close') {
-                if (lastDisconnect.error?.output?.statusCode !== DisconnectReason.loggedOut) {
-                    start();
-                }
-            } else if (connection === 'open') {
-                if (initialConnection) {
-                    console.log(chalk.green("Demon slayer Connected"));
-            Matrix.sendMessage(Matrix.user.id, { 
-                image: { url: "https://files.catbox.moe/5kvvfg.jpg" }, 
-                caption: `╭─────────────━┈⊷
-│ *DARK SAMURAI*
-╰─────────────━┈⊷
+  const validCommands = ['list', 'help', 'menu'];
 
-╭─────────────━┈⊷
-│ *ʙᴏᴛ ᴄᴏɴɴᴇᴄᴛᴇᴅ sᴜᴄᴄᴇssғᴜʟʟʏ*
-│ *ᴘʟᴇᴀsᴇ ғᴏʟʟᴏᴡ ᴜs ʙᴇʟᴏᴡ*
-╰─────────────━┈⊷
- ||https://whatsapp.com/channel/0029VajJoCoLI8YePbpsnE3q||
-> *regards bera tech team*`
-            });
-                    initialConnection = false;
-                } else {
-                    console.log(chalk.blue("♻️ Connection reestablished after restart."));
-                }
-            }
-        });
+  if (validCommands.includes(cmd)) {
+    const str = `╭───❍「 *DARK SAMURAI* 」
+│ 🧑‍💻 *𝚄𝚜𝚎𝚛:* ${pushName} ${pushwish}
+│ 🌐 *𝙼𝚘𝚍𝚎:* ${mode}
+│ ⏰ *𝚃𝚒𝚖𝚎:* ${realTime}
+│ 🚀 *𝚄𝚙𝚃𝚒𝚖𝚎:* ${days}d ${hours}h ${minutes}m ${seconds}s
+╰───────────❍
+╭━━━〔 *DARK SAMURAI* 〕━━━┈⊷
+┃★╭──────────────
+┃★│ Owner : *BRUCE BERA*
+┃★│ User :  ${pushName} ${pushwish}
+┃★│ Baileys : *Multi Device*
+┃★│ Type : *NodeJs*
+┃★│ Mode : *${mode}*
+┃★│ Prefix : [${prefix}]
+┃★│ Version : *1.1.0*
+┃★╰──────────────
+╰━━━━━━━━━━━━━━━┈⊷ 
+> Hey ${pushName} ${pushwish}
+ ╭───❍「 *LOGO MENU* 」
+*┋* ©️ *${prefix}𝗅𝗈𝗀𝗈*
+*┋* ©️ *${prefix}𝖻𝗅𝖺𝖼𝗄𝗉𝗂𝗇𝗄*
+*┋* ©️ *${prefix}𝗀𝗈𝗌𝗌𝗒𝗌𝗂𝗅𝗏𝖾𝗋*
+*┋* ©️ *${prefix}𝗇𝖺𝗋𝗎𝗋𝗈*
+*┋* ©️ *${prefix}𝖽𝗂𝗀𝗂𝗍𝖺𝗅𝗀𝗅𝗂𝗍𝖼𝗁*
+*┋* ©️ *${prefix}𝗉𝗂𝗑𝖾𝗅𝗀𝗅𝗂𝗍𝖼𝗁*
+*┋* ©️ *${prefix}𝗌𝗍𝖺𝗋*
+*┋* ©️ *${prefix}𝗌𝗆𝗈𝗄𝖾*
+*┋* ©️ *${prefix}𝖻𝖾𝖺𝗋*
+*┋* ©️ *${prefix}𝗇𝖾𝗈𝗇𝖽𝖾𝗏𝗂𝗅*
+*┋* ©️ *${prefix}𝗌𝖼𝗋𝖾𝖾𝗇*
+*┋* ©️ *${prefix}𝗇𝖺𝗍𝗎𝗋𝖾*
+*┋* ©️ *${prefix}𝖽𝗋𝖺𝗀𝗈𝗇𝖻𝖺𝗅𝗅*
+*┋* ©️ *${prefix}𝖿𝗈𝗀𝗀𝗒𝗀𝗅𝖺𝗌𝗌*
+*┋* ©️ *${prefix}𝗇𝖾𝗈𝗇𝗅𝗂𝗀𝗁𝗍*
+*┋* ©️ *${prefix}𝖼𝖺𝗌𝗍𝗅𝖾𝗉𝗈𝗉*
+*┋* ©️ *${prefix}𝖿𝗋𝗈𝗓𝖾𝗇𝖼𝗁𝗋𝗂𝗌𝗍𝗆𝖺𝗌*
+*┋* ©️ *${prefix}𝖿𝗈𝗂𝗅𝖻𝖺𝗅𝗅𝗈𝗈𝗇*
+*┋* ©️ *${prefix}𝖼𝗈𝗅𝗈𝗋𝖿𝗎𝗅𝗉𝖺𝗂𝗇𝗍*
+*┋* ©️ *${prefix}𝖺𝗆𝖾𝗋𝗂𝖼𝖺𝗇𝖿𝗅𝖺𝗀*
+*┋* ©️ *${prefix}𝗇𝖾𝗈𝗇𝖽𝖾𝗏𝗂𝗅*
+╭───────────❍
+│DARK SAMURAI 
+╰───────────❍
+╭━❮ 𝙲𝙾𝙽𝚅𝙴𝚁𝚃𝙴𝚁 ❯━╮
+┃◈ ${prefix}𝙰𝚃𝚃𝙿
+┃◈ ${prefix}𝙰𝚃𝚃𝙿2
+┃◈ ${prefix}𝙰𝚃𝚃𝙿3
+┃◈ ${prefix}𝙴𝙱𝙸𝙽𝙰𝚁𝚈
+┃◈ ${prefix}𝙳𝙱𝙸𝙽𝙰𝚁𝚈
+┃◈ ${prefix}𝙴𝙼𝙾𝙹𝙸𝙼𝙸𝚇
+┃◈ ${prefix}𝙼𝙿3
+╰━━━━━━━━━━━━━━━⪼
+╭━❮ 𝙰𝙸 ❯━╮
+┃◈ ${prefix}𝙰𝚒
+┃◈ ${prefix}𝙱𝚞𝚐
+┃◈ ${prefix}𝚁𝚎𝚙𝚘𝚛𝚝
+┃◈ ${prefix}𝙶𝚙𝚝
+┃◈ ${prefix}𝙳𝚊𝚕𝚕𝚎
+┃◈ ${prefix}𝚁𝚎𝚖𝚒𝚗𝚒
+┃◈ ${prefix}𝙶𝚎𝚖𝚒𝚗𝚒
+╰━━━━━━━━━━━━━━━⪼
+╭━❮ 𝚃𝙾𝙾𝙻 ❯━╮
+┃◈ ${prefix}𝙲𝚊𝚕𝚌𝚞𝚕𝚊𝚝𝚘𝚛
+┃◈ ${prefix}𝚃𝚎𝚖𝚙𝚖𝚊𝚒𝚕
+┃◈ ${prefix}𝙲𝚑𝚎𝚌𝚔𝚖𝚊𝚒𝚕
+┃◈ ${prefix}𝚃𝚛𝚝
+┃◈ ${prefix}𝚃𝚝𝚜
+╰━━━━━━━━━━━━━━━⪼
+╭━❮ 𝙶𝚁𝙾𝚄𝙿 ❯━╮
+┃◈ ${prefix}𝙻𝚒𝚗𝚔𝙶𝚛𝚘𝚞𝚙
+┃◈ ${prefix}𝚂𝚎𝚝𝚙𝚙𝚐𝚌
+┃◈ ${prefix}𝚂𝚎𝚝𝚗𝚊𝚖𝚎
+┃◈ ${prefix}𝚂𝚎𝚝𝚍𝚎𝚜𝚌
+┃◈ ${prefix}𝙶𝚛𝚘𝚞𝚙
+┃◈ ${prefix}𝙶𝚌𝚜𝚎𝚝𝚝𝚒𝚗𝚐
+┃◈ ${prefix}𝚆𝚎𝚕𝚌𝚘𝚖𝚎
+┃◈ ${prefix}𝙰𝚍𝚍
+┃◈ ${prefix}𝙺𝚒𝚌𝚔
+┃◈ ${prefix}𝙷𝚒𝚍𝚎𝚃𝚊𝚐
+┃◈ ${prefix}𝚃𝚊𝚐𝚊𝚕𝚕
+┃◈ ${prefix}𝙰𝚗𝚝𝚒𝙻𝚒𝚗𝚔
+┃◈ ${prefix}𝙰𝚗𝚝𝚒𝚃𝚘𝚡𝚒𝚌
+┃◈ ${prefix}𝙿𝚛𝚘𝚖𝚘𝚝𝚎
+┃◈ ${prefix}𝙳𝚎𝚖𝚘𝚝𝚎
+┃◈ ${prefix}𝙶𝚎𝚝𝚋𝚒𝚘
+╰━━━━━━━━━━━━━━━⪼
+╭━❮ 𝙳𝙾𝚆𝙽𝙻𝙾𝙰𝙳 ❯━╮
+┃◈ ${prefix}𝙰𝚙𝚔
+┃◈ ${prefix}𝙵𝚊𝚌𝚎𝚋𝚘𝚘𝚔
+┃◈ ${prefix}𝙼𝚎𝚍𝚒𝚊𝚏𝚒𝚛𝚎
+┃◈ ${prefix}𝙿𝚒𝚗𝚝𝚎𝚛𝚎𝚜𝚝𝚍𝚕
+┃◈ ${prefix}𝙶𝚒𝚝𝚌𝚕𝚘𝚗𝚎
+┃◈ ${prefix}𝙶𝚍𝚛𝚒𝚟𝚎
+┃◈ ${prefix}𝙸𝚗𝚜𝚝𝚊
+┃◈ ${prefix}𝚈𝚝𝚖𝚙3
+┃◈ ${prefix}𝚈𝚝𝚖𝚙4
+┃◈ ${prefix}𝙿𝚕𝚊𝚢
+┃◈ ${prefix}𝚂𝚘𝚗𝚐
+┃◈ ${prefix}𝚅𝚒𝚍𝚎𝚘
+┃◈ ${prefix}𝚈𝚝𝚖𝚙3𝚍𝚘𝚌
+┃◈ ${prefix}𝚈𝚝𝚖𝚙4𝚍𝚘𝚌
+┃◈ ${prefix}𝚃𝚒𝚔𝚝𝚘𝚔
+╰━━━━━━━━━━━━━━━⪼
+╭━❮ 𝚂𝙴𝙰𝚁𝙲𝙷 ❯━╮
+┃◈ ${prefix}𝙿𝚕𝚊𝚢
+┃◈ ${prefix}𝚈𝚝𝚜
+┃◈ ${prefix}𝙸𝚖𝚍𝚋
+┃◈ ${prefix}𝙶𝚘𝚘𝚐𝚕𝚎
+┃◈ ${prefix}𝙶𝚒𝚖𝚊𝚐𝚎
+┃◈ ${prefix}𝙿𝚒𝚗𝚝𝚎𝚛𝚎𝚜𝚝
+┃◈ ${prefix}𝚆𝚊𝚕𝚕𝚙𝚊𝚙𝚎𝚛
+┃◈ ${prefix}𝚆𝚒𝚔𝚒𝚖𝚎𝚍𝚒𝚊
+┃◈ ${prefix}𝚈𝚝𝚜𝚎𝚊𝚛𝚌𝚑
+┃◈ ${prefix}𝚁𝚒𝚗𝚐𝚝𝚘𝚗𝚎
+┃◈ ${prefix}𝙻𝚢𝚛𝚒𝚌𝚜
+╰━━━━━━━━━━━━━━━⪼
+╭━❮ 𝙼𝙰𝙸𝙽 ❯━╮
+┃◈ ${prefix}𝙿𝚒𝚗𝚐
+┃◈ ${prefix}𝙰𝚕𝚒𝚟𝚎
+┃◈ ${prefix}𝙾𝚠𝚗𝚎𝚛
+┃◈ ${prefix}𝙼𝚎𝚗𝚞
+┃◈ ${prefix}𝙸𝚗𝚏𝚘𝚋𝚘𝚝
+╰━━━━━━━━━━━━━━━⪼
+╭━❮ 𝙾𝚆𝙽𝙴𝚁 ❯━╮
+┃◈ ${prefix}𝙹𝚘𝚒𝚗
+┃◈ ${prefix}𝙻𝚎𝚊𝚟𝚎
+┃◈ ${prefix}𝙱𝚕𝚘𝚌𝚔
+┃◈ ${prefix}𝚄𝚗𝚋𝚕𝚘𝚌𝚔
+┃◈ ${prefix}𝚂𝚎𝚝𝚙𝚙𝚋𝚘𝚝
+┃◈ ${prefix}𝙰𝚗𝚝𝚒𝚌𝚊𝚕𝚕
+┃◈ ${prefix}𝚂𝚎𝚝𝚜𝚝𝚊𝚝𝚞𝚜
+┃◈ ${prefix}𝚂𝚎𝚝𝚗𝚊𝚖𝚎𝚋𝚘𝚝
+┃◈ ${prefix}𝙰𝚞𝚝𝚘𝚃𝚢𝚙𝚒𝚗𝚐
+┃◈ ${prefix}𝙰𝚕𝚠𝚊𝚢𝚜𝙾𝚗𝚕𝚒𝚗𝚎
+┃◈ ${prefix}𝙰𝚞𝚝𝚘𝚁𝚎𝚊𝚍
+┃◈ ${prefix}𝚊𝚞𝚝𝚘𝚜𝚟𝚒𝚎𝚠
+╰━━━━━━━━━━━━━━━⪼
+╭━❮ 𝚂𝚃𝙰𝙻𝙺 ❯━╮
+┃◈ ${prefix}𝚃𝚛𝚞𝚎𝚌𝚊𝚕𝚕𝚎𝚛
+┃◈ ${prefix}𝙸𝚗𝚜𝚝𝚊𝚂𝚝𝚊𝚕𝚔
+┃◈ ${prefix}𝙶𝚒𝚝𝚑𝚞𝚋𝚂𝚝𝚊𝚕𝚔
+╰━━━━━━━━━━━━━━━⪼`;
 
-        Matrix.ev.on('creds.update', saveCreds);
-
-        Matrix.ev.on("messages.upsert", async chatUpdate => await Handler(chatUpdate, Matrix, logger));
-        Matrix.ev.on("call", async (json) => await Callupdate(json, Matrix));
-        Matrix.ev.on("group-participants.update", async (messag) => await GroupUpdate(Matrix, messag));
-
-        if (config.MODE === "public") {
-            Matrix.public = true;
-        } else if (config.MODE === "private") {
-            Matrix.public = false;
-        }
-
-        Matrix.ev.on('messages.upsert', async (chatUpdate) => {
-            try {
-                const mek = chatUpdate.messages[0];
-                if (!mek.key.fromMe && config.AUTO_REACT) {
-                    console.log(mek);
-                    if (mek.message) {
-                        const randomEmoji = emojis[Math.floor(Math.random() * emojis.length)];
-                        await doReact(randomEmoji, mek, Matrix);
-                    }
-                }
-            } catch (err) {
-                console.error('Error during auto reaction:', err);
-            }
-        });
-    } catch (error) {
-        console.error('Critical Error:', error);
-        process.exit(1);
-    }
-}
-
-async function init() {
-    if (fs.existsSync(credsPath)) {
-        console.log("🔒 Session file found, proceeding without QR code.");
-        await start();
-    } else {
-        const sessionDownloaded = await downloadSessionData();
-        if (sessionDownloaded) {
-            console.log("🔒 Session downloaded, starting bot.");
-            await start();
-        } else {
-            console.log("No session found or downloaded, QR code will be printed for authentication.");
-            useQR = true;
-            await start();
-        }
-    }
-}
-
-init();
-
-app.get('/', (req, res) => {
-    res.send('am joel bot');
-});
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
-
-
-// updated by lord joel 
-
+    const sendCommandMessage = async (messageContent) => {
+    await sock.sendMessage(
+      m.from,
+      {
+        text: messageContent,
+        contextInfo: {
+          isForwarded: true,
+          forwardingScore: 999,
+          forwardedNewsletterMessageInfo: {
+            newsletterJid: '120363315115438245@newsletter', // Preserved newsletter JID
+            newsletterName: "ʀᴇɢᴀʀᴅs ʙᴇʀᴀ ᴛᴇᴄʜ",
+            serverMessageId: -1,
+          },
+          externalAdReply: {
+            title: "",
+            body: pushName,
+            thumbnailUrl: 'https://files.catbox.moe/7xgzln.jpg', // Thumbnail URL
+            sourceUrl: 'https://files.catbox.moe/nk3nuz.mp3', // Source URL
+            mediaType: 1,
+            renderLargerThumbnail: true,
+          },
+        },
+      },
+      { quoted: m }
+    );
+  };
